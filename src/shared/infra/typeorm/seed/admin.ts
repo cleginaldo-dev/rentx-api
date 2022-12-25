5ec2d739bef6f7386796ed97ed6af93565b1d7ac
Cleginaldo <cleginaldo3000@gmail.com>
2022-12-25 14:27:36 -0300

    refactor: refactore integrations tests

--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -1,21 +1,21 @@
 import { hash } from "bcryptjs";
-import { v4 as uuid } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 import createConnection from "../index";
 
 async function create() {
   const connection = await createConnection("localhost");
 
-  const id = uuid();
+  const id = uuidv4();
   const password = await hash("admin", 8);
 
   await connection.query(
-    `INSERT INTO USERS(id, name, username, email, password, "admin", created_at, driver_license )
-    values('${id}', 'admin', 'rentxAdmin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXX-XXXX')
-    `
+    `INSERT INTO USERS(id, name, email, password, "admin", created_at, driver_license)
+            values('${id}', 'admin', 'admin@rentx.com.br', '${password}', 'true', 'now()', 'XXXXX')
+        `
   );
 
   await connection.close();
 }
 
-create().then(() => console.log("User admin created"));
+create().then(() => console.log("user admin created!"));
